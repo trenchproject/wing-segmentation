14eb1addeb156769dc13aaf2a7e33e67a3d66ff8
Michelle Ramirez <ramirez528@owens-rw02.ten.osc.edu>
2023-11-28 09:38:59 -0500

    added scripts to flip imgs for landmarks

---
+++ landmark_scripts/create_wing_folders.py
@@ -0,0 +1,71 @@
+import os
+import glob
+import cv2
+import matplotlib.pyplot as plt
+import argparse
+
+CLASSES = {0: 'background',
+        1: 'generic',
+        2: 'right_forewing',
+        3: 'left_forewing',
+        4: 'right_hindwing',
+        5: 'left_hindwing',
+        6: 'ruler',
+        7: 'white_balance',
+        8: 'label',
+        9: 'color_card',
+        10: 'body'}
+
+
+def write_images_to_wing_folders(main_folder):
+    #go through each species subfolder
+    for directory_path in glob.glob(main_folder + '/*'):
+        print(directory_path)
+        #go through each image in the current species subfolder
+        for img_path in glob.glob(os.path.join(directory_path, "*.png")):
+
+            #create the new path to save the image under its wing folder
+            wing_class = int(img_path.split('_wing_')[-1].split('.png')[0])
+            wing_folder = CLASSES[wing_class]
+            image_name = img_path.split('_wing_')[0].split('/')[-1]
+            wing_path = f"{main_folder}_{wing_folder}/{image_name}.png"
+            print('wing path:', wing_path)
+            
+            #load the actual image file
+            img = cv2.imread(img_path, 0)
+
+            #save the actual image in the new location
+            saved_img = cv2.imwrite(wing_path, img)
+    return
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_dir", required=True, help="Path to folder containing cropped wing images. Ex. /Users/michelle/cropped_wings")
+    return parser.parse_args()
+
+def main():
+
+    args = parse_args()
+
+    #directory of where all the cropped wings (not seperated by type of wing) are saved
+    cropped_wings_dir = args.input_dir
+    # '/Users/michelleramirez/Documents/butterflies/ml-morph/jiggins-image-examples/cropped_wings_256_256/jiggins/jiggins_data_256_256/*'
+
+    #create seperate directories for each type of wing, which we will seperate our wing images into
+    right_forewings = args.input_dir + '_right_forewing' #'/Users/michelleramirez/Documents/butterflies/ml-morph/jiggins-image-examples/right_forewing'
+    right_hindwings = args.input_dir + '_right_hindwing' #'/Users/michelleramirez/Documents/butterflies/ml-morph/jiggins-image-examples/right_hindwing'
+    left_forewings  = args.input_dir + '_left_forewing'  #'/Users/michelleramirez/Documents/butterflies/ml-morph/jiggins-image-examples/left_forewing'
+    left_hindwings  = args.input_dir + '_left_hindwing'  #'/Users/michelleramirez/Documents/butterflies/ml-morph/jiggins-image-examples/left_hindwing'
+
+    os.makedirs(right_forewings, exist_ok=True)
+    os.makedirs(right_hindwings, exist_ok=True)
+    os.makedirs(left_forewings, exist_ok=True)
+    os.makedirs(left_hindwings, exist_ok=True)
+
+    #resave images in the cropped_wings_dir to their respective folders based on their titles
+    write_images_to_wing_folders(cropped_wings_dir)
+
+    return
+
+if __name__ == "__main__":
+    main() 
---
+++ landmark_scripts/flip_images_horizontally.py
@@ -0,0 +1,50 @@
+import os
+import glob
+import cv2
+import matplotlib.pyplot as plt
+import argparse
+
+
+def flip_images(source_folder, dest_folder):
+    #go through each species subfolder
+    for img_path in glob.glob(os.path.join(source_folder, "*.png")):
+
+        #create the new path to save the image under its wing folder
+        wing_path = img_path.replace(source_folder, dest_folder)
+        print(wing_path)
+        
+        #load the actual image file
+        img = cv2.imread(img_path, 0)
+
+        #flip the image
+        img_h = cv2.flip(img, 1)
+
+        #save the flipped image in the new location
+        saved_img = cv2.imwrite(wing_path, img_h)
+    return
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_dir", required=True, help="Path to folder containing cropped wing images. Ex. /Users/michelle/cropped_wings")
+    return parser.parse_args()
+
+def main():
+
+    args = parse_args()
+
+    ## Note: for our MLmorph models, we need to flip the left fw's and left hw's only
+    # left_forewings = '/Users/michelleramirez/Documents/butterflies/Appendometer/jiggins-image-examples/left_forewing'
+    # left_forewings_flipped = '/Users/michelleramirez/Documents/butterflies/Appendometer/jiggins-image-examples/left_forewing_flipped'
+
+    #directories where flipped images will be stored
+    flipped = args.input_dir + '_flipped'
+    os.makedirs(flipped, exist_ok=True)
+
+    #resave images in the cropped_wings_dir to their respective folders based on their titles
+    flip_images(args.input_dir, flipped)
+
+    return
+
+if __name__ == "__main__":
+    main() 
