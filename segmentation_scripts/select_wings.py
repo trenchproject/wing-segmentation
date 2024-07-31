e611ab9f44197b862eafe814bc42e78a2d0779d5
micheller7171 <michelleramirez7171@gmail.com>
2024-07-31 12:27:40 -0400

    adding yolo_sam scripts

---
+++ segmentation_scripts/select_wings.py
@@ -0,0 +1,88 @@
+import os
+import glob
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import argparse
+
+from utils import read_image_paths, load_batch_images
+
+def batch_indices_np(total, batch_size):
+    indices = np.arange(total)
+    return np.array_split(indices, np.ceil(total / batch_size))
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--images", required=True, help="Directory containing images we have masks for. ex: /User/micheller/data/jiggins_256_256")
+    parser.add_argument("--masks", required=True, help="Directory containing masks for images.")
+    parser.add_argument("--main_folder", required=True, help="JUST the main FOLDER NAME containing all subfolders/images. ex: jiggins_256_256")
+    
+    return parser.parse_args()
+
+def remove_background_color_based(image, mask):
+    # Create an image with white background
+    white_background = np.full(image.shape, 255, dtype=np.uint8)
+
+    # Apply the mask to the white background, where mask is not zero, we will fill it with the image's color
+    masked_foreground = cv2.bitwise_and(image, image, mask=mask) #mask=255-mask #wings
+    background = cv2.bitwise_and(white_background, white_background, mask=mask)
+    background = 255 - background
+
+    # Combine the masked foreground with the background
+    final_image = cv2.add(masked_foreground, background)
+    return final_image
+
+def main():
+    args = parse_args()
+
+    # load in our images
+    print('Reading in images...')
+    image_dataset_folder = args.images + '/*'
+    image_filepaths = read_image_paths(image_dataset_folder)
+
+    #load in our masks
+    print('Reading in masks...')
+    mask_dataset_folder = args.masks + '/*'
+    mask_filepaths = read_image_paths(mask_dataset_folder)
+
+    #create batches of 16
+    batches = batch_indices_np(len(image_filepaths), 16)
+    
+    print('Removing everything but forewings and hindwings...')
+    errors = []
+    for i, batch in enumerate(batches[76:]):
+        print(f'Batch {i+76}: ', batch)
+        img_batch_fps = [image_filepaths[i] for i in batch]
+        mask_batch_fps = [mask_filepaths[i] for i in batch]
+
+        dataset_images = load_batch_images(img_batch_fps, 1)
+        dataset_masks = load_batch_images(mask_batch_fps)
+
+        for (image, mask), fp in zip(zip(dataset_images, dataset_masks), img_batch_fps): 
+            print(fp)
+            # subselect the wing categories in our mask
+            wing_mask = 4* (mask == 4) | 3 * (mask == 3) | 2 * (mask == 2) | 1 *(mask == 1)
+            wing_mask  = wing_mask.astype(np.uint8)
+
+            #add a white background to wings
+            img_removed_background = remove_background_color_based(image, wing_mask)
+
+            #save the image with the removed background under its own folder
+            bck_img_path = fp.replace(args.main_folder, f'{args.main_folder}_wings_on_white_background')
+            fn = "/" + bck_img_path.split('/')[-1]
+            background_folder = bck_img_path.replace(fn, "")
+            os.makedirs(background_folder, exist_ok=True)
+
+            #save the resized cropped wings to their path
+            try:
+                cv2.imwrite(bck_img_path, cv2.cvtColor(img_removed_background, cv2.COLOR_RGB2BGR))
+            except FileNotFoundError:
+                errors.append(bck_img_path)
+            
+    print('The following images could encountered errors during background removal:', errors)
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ segmentation_scripts/yolo_sam_predict_mask.py
@@ -0,0 +1,179 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+import torch
+import cv2
+import matplotlib.pyplot as plt
+from ultralytics import YOLO
+from segment_anything import sam_model_registry, SamPredictor
+import torch, torchvision
+import argparse
+
+from utils import load_dataset_images, read_image_paths
+
+def get_mask(r, image_path, predictor):
+    # read in image
+    image = cv2.imread(image_path, 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # configurate SAM model to use current image
+    predictor.set_image(image)
+
+    # get bbox info from our yolo result
+    bboxes_labels = r.boxes.cls
+    bboxes_xyxy = r.boxes.xyxy
+
+    # accumulate all mask componenets into a single mask
+    img_mask = np.zeros((image.shape[0], image.shape[1]))
+    for idx, bbox_label in enumerate(bboxes_labels):
+        input_box = np.array(bboxes_xyxy[idx].tolist())
+        pred_label = bbox_label.item()
+        
+        mask, _, _ = predictor.predict(
+            point_coords=None,
+            point_labels=None,
+            box=input_box[None, :],
+            multimask_output=False,
+        )
+        mask = mask.squeeze()
+        mask = mask * pred_label
+        img_mask += mask
+        
+        # resolve overlapping mask values
+        overlapping_pixels = (mask != 0) & (img_mask != 0)
+        img_mask[overlapping_pixels] = mask[overlapping_pixels]
+        
+    img_mask = img_mask.astype(np.uint8)
+    return img_mask
+
+
+def get_sam_model(device):
+    '''Get the SAM VIT l Model'''
+    model_type = "vit_l"
+    sam_checkpoint = "/fs/ess/PAS2136/Butterfly/butterfly_image_segmentation/detection-segmentation/sam_vit_l_0b3195.pth"
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam.to(device=device)
+    return SamPredictor(sam)
+
+def get_yolo_model():
+    '''Download trained yolo v8 model from huggingface and load in weights'''
+
+    ## check if we already have the trained yolo checkpoint file
+    if os.path.exists("/fs/ess/PAS2136/Butterfly/butterfly_image_segmentation/yolo-wing-detection/yolo_models/yolov8m_shear_10.0_scale_0.5_translate_0.1_fliplr_0.0/weights/best.pt"):
+        checkpoint = "/fs/ess/PAS2136/Butterfly/butterfly_image_segmentation/yolo-wing-detection/yolo_models/yolov8m_shear_10.0_scale_0.5_translate_0.1_fliplr_0.0/weights/best.pt"
+    else:
+        print('Need to upload model to HuggingFace')
+        # model_url = "https://huggingface.co/imageomics/butterfly_segmentation_yolo_v8/resolve/main/yolov8m_shear_10.0_scale_0.5_translate_0.1_fliplr_0.0_best.pt"
+        # checkpoint = wget.download(model_url)
+
+    model = YOLO(checkpoint)
+    return model
+
+def batch_indices_np(total, batch_size):
+    indices = np.arange(total)
+    return np.array_split(indices, np.ceil(total / batch_size))
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", required=True, help="Directory containing images we want to predict masks for. ex: /User/micheller/data/jiggins_256_256")
+    parser.add_argument("--mask_csv", required=False, default = 'dataset_segmentation_info.csv', help="Path to the csv created containing \
+                        which segmentation classes are present in each image's predicted mask.")
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    classes = {0: 'background',
+                1: 'right_forewing',
+                2: 'left_forewing',
+                3: 'right_hindwing',
+                4: 'left_hindwing',
+                5: 'ruler',
+                6: 'white_balance',
+                7: 'label',
+                8: 'color_card',
+                9: 'body'}
+    
+    # Leverage GPU if available
+    use_cuda = torch.cuda.is_available()
+    DEVICE   = torch.device("cuda:0" if use_cuda else "cpu")
+    print("Device: ", DEVICE)
+
+    if use_cuda:
+        print('__CUDNN VERSION:', torch.backends.cudnn.version())
+        print('__Number CUDA Devices:', torch.cuda.device_count())
+        print('__CUDA Device Name:',torch.cuda.get_device_name(0))
+        print('__CUDA Device Total Memory [GB]:',torch.cuda.get_device_properties(0).total_memory/1e9)
+    
+    # read images in
+    dataset_folder = args.dataset + '/*'
+    image_filepaths = read_image_paths(dataset_folder) #just read in image filepaths since model handles converting path to image
+    print(f'Number of images in dataset: {len(image_filepaths)}')
+
+    dataset_segmented = pd.DataFrame(columns = ['image', 'background', 
+                                            'right_forewing', 'left_forewing', 
+                                            'right_hindwing', 'left_hindwing', 
+                                            'ruler', 'white_balance', 
+                                            'label', 'color_card', 'body'])
+    
+
+    # load trained yolo detection model
+    bbox_model = get_yolo_model()
+
+    # load SAM model
+    segmentation_model = get_sam_model(DEVICE)
+
+    i = 0 #df indexer
+    batches = batch_indices_np(len(image_filepaths), 16)
+    for batch in batches: 
+        print('Current batch:', batch)
+        image_fp_batch = [image_filepaths[i] for i in batch]
+
+        # predict bboxes for all components in our images 
+        results = bbox_model.predict(image_fp_batch, verbose=False)
+    
+        # create masks using SAM (segment-anything-model)
+        print("Using YOLO BBOX to Get SAM Mask...")
+        for r, fp in zip(results, image_fp_batch):
+            #get the mask with category id's as pixel values
+            mask = get_mask(r, fp, segmentation_model) 
+            
+            #create the path to which the mask will be saved, replicating the folder + naming structure of the input dataset
+            mask_path = fp.replace(args.dataset, f'{args.dataset}_masks')
+            mask_path = mask_path.replace(f".{fp.split('.')[-1]}", "_mask.png") #replace extension and save mask as a png
+            
+            #create the folder in which the mask will be saved in if it doesn't exist already
+            mask_filename = "/" + mask_path.split('/')[-1] #
+            mask_folder = mask_path.replace(mask_filename, "")
+            os.makedirs(mask_folder, exist_ok=True)
+            
+            #save mask with cv2 to preserve pixel categories
+            print(f"Mask path:{mask_path}")
+            cv2.imwrite(mask_path, mask)
+
+            #enter relevant segmentation data for the image in our dataframe
+            classes_in_image = np.unique(mask)
+            print('Classes in image:', classes_in_image)
+            classes_not_in_image = set(classes.keys()) ^ set(classes_in_image)
+            dataset_segmented.loc[i, 'image'] = fp
+            
+            #enter `1` for all segmentation classes that appear in our mask
+            for val in classes_in_image:
+                pred_class = classes[val]
+                dataset_segmented.loc[i, pred_class] = 1 #class exists in segmentation mask
+
+            #enter `0` for all segmentation classes that were not predicted
+            for not_pred_val in classes_not_in_image:
+                not_pred_class = classes[not_pred_val]
+                dataset_segmented.loc[i, not_pred_class] = 0 #class does not exist in segmentation mask
+
+            i += 1
+
+    # Save csv containing information about segmentation masks per each image
+    dataset_segmented.to_csv(args.segmentation_csv, index=False)
+
+    return
+
+if __name__ == "__main__":
+    main()
